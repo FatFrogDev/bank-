ab6f26339e1b2270a8cd00ff44c5c5c412d2a053
“JuanSeGA” <117958247+JuanSeGArtor@users.noreply.github.com>
2024-01-04 13:36:42 -0500

    agregar menu a Main y definir metodos basicos a Client

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,66 @@
-public class Client extends User{
+import java.util.List;
 
+public class Client extends User{
+    private int id;
     private String name;
-    private int documentNumber;
+    private int document;
+    private List<BankProduct> products;
+
+    // Getters & Setters
+    public Client(int id, String name, int document, List<BankProduct> products) {
+        this.id = id;
+        this.name = name;
+        this.document = document;
+        this.products = products;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDocument() {
+        return document;
+    }
+
+    public void setDocument(int document) {
+        this.document = document;
+    }
+
+    public List<BankProduct> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<BankProduct> products) {
+        this.products = products;
+    }
+
+    // Mostrar productos del cliente
+    public void showProducts() {
+        System.out.println("Productos del cliente: ");
+        for (BankProduct product : products) {
+            System.out.println(product);
+        }
+    }
+
+
+    // Mostrar info del cliente
+    public void showPersonalInfo() {
+        System.out.println("Tu Perfil:");
+        System.out.println("Nombre: " + getName());
+        System.out.println("Documento: " + getDocument());
+        System.out.println("Correo: ");
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        Client client = new Client();
+
+        int op;
+        do{
+            System.out.println("Banco Menú");
+            System.out.println("Operaciones del cliente");
+            System.out.println("(1) Mostrar información personal");
+            System.out.println("(2) Modificar información personal");
+            System.out.println("(3) Mostrar productos del cliente");
+            System.out.println("(4) Modificar cuenta");
+            System.out.println("(0) Salir");
+            op = sc.nextInt();
+            sc.nextLine();
+
+            switch(op) {
+                case 1:
+                    client.showPersonalInfo();
+                    break;
+                case 2:
+                    client.modifyPersonalInfo();
+                    break;
+                case 3:
+                    client.showProducts();
+                    break;
+                case 4:
+                    client.modifyAccount();
+                    break;
+            }
+        }while(op!=0);
+
+        sc.close();
     }
 }
